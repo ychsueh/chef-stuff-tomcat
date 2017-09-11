205681b67c9658359738ca15332ec66b913d9dad
Cloud User <ec2-user@ip-172-31-27-95.us-west-2.compute.internal>
2017-09-11 06:10:22 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# learn_chef_tomcat
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: learn_chef_tomcat
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
